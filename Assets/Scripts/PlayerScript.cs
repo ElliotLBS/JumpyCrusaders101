6258243b0d0a095036b05650ee8e1dcdc9f11fb3
ElliotLBS <98454374+ElliotLBS@users.noreply.github.com>
2022-05-20 11:23:42 +0200

    FixadCountingtext

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -113,11 +113,6 @@ public class PlayerScript : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Coin") //om man kolliderar med nyckeln (trigger) s� blir "hasKey" true
-        {
-            countingcoins.Counting += 1;
-            print("Got key!");
-        }
 
         //Gjord av Simon
         #region Key
