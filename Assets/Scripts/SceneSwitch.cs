6cc81900e6e9c7721ee473f1732f9b0cce4bb997
GamingPhoenix04 <marcus.picha@elev.ga.lbs.se>
2022-06-01 10:02:18 +0200

    aa

--- Assets/Scripts/SceneSwitch.cs
+++ Assets/Scripts/SceneSwitch.cs
@@ -8,7 +8,7 @@ public class SceneSwitch : MonoBehaviour
     //gjord av Simon
     private void OnTriggerEnter2D(Collider2D collision) //när man nuddar ett specifikt block så byter man till nästa scen (slutet)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 6);
 
     }
 }
